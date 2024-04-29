e1f142011c9ae928e618dc2295e8a8b08a7c9e19
Gauthier Piarrette <gauthierpiarrette@gmail.com>
2024-04-30 00:04:03 +0200

    add feature to use different providers

--- akita/services/text_generation/text_generator.py
+++ akita/services/text_generation/text_generator.py
@@ -1,32 +1,28 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, List, Optional, Union
+import os
 from akita.services.text_generation.prompt_builder import PromptBuilder
 from akita.services.text_generation.file_handler import FileHandler
-from akita.api.openai_client import OpenAIClient
+from akita.api.base_ai_provider import AIProvider
+from akita.api.provider_factory import ProviderFactory
 from .settings import Settings
-from .utils import load_model_config
-import os
 
 
 class TextGenerator:
     """
-    Generates various types of text content using OpenAI's GPT models,
+    Generates various types of text content using AI models,
     based on the provided code content.
 
     Attributes:
-        model_config (Dict[str, Any]): Configuration for the models
-                                       used in generating text.
         file_handler (FileHandler): Handles file operations,
                                     like reading files to get their content.
-        chatgpt_client (OpenAIClient): Client for calling OpenAI's API to generate text.
+        ai_provider (AIProvider): An AI provider conforming to the AIProvider interface,
+                              used to generate text.
         prompt_builder (PromptBuilder): Builds prompts for the AI based on templates.
     """
 
     def __init__(self) -> None:
-        self.model_config: Dict[str, Any] = load_model_config(
-            Settings.MODEL_CONFIG_FILE
-        )
         self.file_handler: FileHandler = FileHandler()
-        self.chatgpt_client: OpenAIClient = OpenAIClient()
+        self.ai_provider: AIProvider = ProviderFactory.get_provider()
         self.prompt_builder: PromptBuilder = PromptBuilder(
             prompts_dir=Settings.DEFAULT_PROMPT_DIR
         )
@@ -34,114 +30,90 @@ class TextGenerator:
     def generate_docstring(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("docstring", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=3000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=3000)
 
     def generate_inline_comments(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("inline_comments", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=4000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=4000)
 
     def generate_description_files(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("describe_files", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=4000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=4000)
 
     def generate_description_code_diff(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("describe_code_diff", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=500, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=500)
 
     def generate_commit_message(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("commit_message", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=60, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=60)
 
     def generate_readme(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("readme", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=3000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=3000)
 
     def generate_review(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("review", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=3000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=3000)
 
     def generate_tests(
         self,
         input_data: Union[str, List[str]],
-        model: str = "default",
         verbosity: Optional[str] = None,
         language: Optional[str] = None,
     ) -> Any:
         self._prepare_prompt_builder(verbosity, language)
         code_content: str = self._process_input(input_data)
         prompt: str = self.prompt_builder.get_prompt("tests", code_content)
-        return self.chatgpt_client.call_openai_api(
-            prompt, max_tokens=3000, model=self.model_config[model]
-        )
+        return self.ai_provider.call_api(prompt, max_tokens=3000)
 
     def _prepare_prompt_builder(
         self, verbosity: Optional[str], language: Optional[str]
